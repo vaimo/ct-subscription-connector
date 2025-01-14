ddde0989919581679db424f8f921d0e144cafa17
christos.stergianos <christos.stergianos@vaimo.com>
2025-01-14 14:52:12 +0000

    Add working connect for subscriptions

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -2,64 +2,70 @@ import { UpdateAction } from '@commercetools/sdk-client-v2';
 import CustomError from '../errors/custom.error';
 import { Resource } from '../interfaces/resource.interface';
 import fetch from 'node-fetch';
+import { format } from 'date-fns'; // For date formatting
+
+interface CreateResponse {
+    statusCode: number;
+    actions: Array<UpdateAction>;
+}
 
 /**
  * Handle the create action for orders
  *
  * @param {Resource} resource The resource from the request body
- * @returns {object}
+ * @returns {CreateResponse}
  */
-const create = async (resource: Resource) => {
+const create = async (resource: Resource): Promise<CreateResponse> => {
     const orderDraft = JSON.parse(JSON.stringify(resource));
     const updateActions: Array<UpdateAction> = [];
+    const subscriptionTypeId = "5af7be9a-0895-4c20-b881-8039b7a9e271";
 
-    const hasSubscriptionProduct = orderDraft.obj.lineItems.some((lineItem: any) => {
-        return lineItem.productType?.obj?.key === 'subscription';
-    });
+    const promises = orderDraft.obj.lineItems
+        .filter((lineItem: any) => lineItem.productType.id === subscriptionTypeId)
+        .map(async (lineItem: any) => {
+            const orderData = {
+                customerId: orderDraft.obj.customerId,
+                productId: lineItem.id,
+                status: true,
+                frequency: '',
+                startDate: '',
+                endDate: ''
+            };
 
-    if (!hasSubscriptionProduct) {
-        return { statusCode: 200, message: 'No subscription products in the order' };
+            lineItem.variant.attributes.forEach((attribute: any) => {
+                switch (attribute.name) {
+                    case 'frequency':
+                        orderData.frequency = attribute.value.label.toLowerCase();
+                        break;
+                    case 'startDate':
+                        orderData.startDate = format(new Date(attribute.value), 'dd-MM-yyyy');
+                        break;
+                    case 'endDate':
+                        orderData.endDate = format(new Date(attribute.value), 'dd-MM-yyyy');
+                        break;
+                }
+            });
+            
+            const response = await fetch('https://ct-subscription.fly.dev/subscriptions/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(orderData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Fly.io API call failed: ${response.statusText}`);
+            }
+        });
+
+    try {
+        await Promise.all(promises);
+    } catch (error: any) {
+        throw new CustomError(500, `Failed to process order: ${error.message}`);
     }
 
-    // Prepare the data to send to the Fly.io HTTPS API
-    const orderData = {
-        orderId: orderDraft.id,
-        customerId: orderDraft.obj.customerId,
-        lineItems: orderDraft.obj.lineItems,
-    };
-
-    console.log(orderDraft);
-
-    return { statusCode: 200, actions: updateActions }
-
-    // try {
-    //     // Call the Fly.io API
-    //     const response = await fetch('https://ct-subscription.fly.dev/subscriptions/add', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify(orderData),
-    //     });
-    //
-    //     if (!response.ok) {
-    //         throw new Error(`Fly.io API call failed: ${response.statusText}`);
-    //     }
-    //
-    //     // Add an update action to mark the order as processed
-    //     const updateAction: UpdateAction = {
-    //         action: 'addCustomField',
-    //         name: 'processedByExtension',
-    //         value: true,
-    //     };
-    //
-    //     updateActions.push(updateAction);
-    //
-    //     return { statusCode: 201, actions: updateActions };
-    // } catch (error: any) {
-    //     console.error('Error calling Fly.io API:', error.message);
-    //     throw new CustomError('ApiError', 500, `Failed to process order: ${error.message}`);
-    // }
+    return { statusCode: 200, actions: updateActions };
 };
 
 /**
@@ -67,12 +73,12 @@ const create = async (resource: Resource) => {
  *
  * @param {string} action The action that comes with the request. Could be `Create` or `Update`
  * @param {Resource} resource The resource from the request body
- * @returns {Promise<object>} The data from the method that handles the action
+ * @returns {Promise<CreateResponse>} The data from the method that handles the action
  */
 export const orderController = async (
     action: string,
     resource: Resource
-): Promise<object> => {
+): Promise<CreateResponse> => {
     switch (action) {
         case 'Create': {
             return await create(resource);
